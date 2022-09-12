bb9c6fedfad44a6cc8ce8640372ad28d241159fe
Arthur Korostelev <korostelev.art.ua@gmail.com>
2022-09-12 14:39:08 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,32 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    backdrop: document.querySelector('.backdrop'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", openModal);
+  refs.closeModalBtn.addEventListener("click", closeModal);
+  refs.backdrop.addEventListener("click", onBackdropClick);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  function openModal() {
+    window.addEventListener("keydown", onEscapePress);
+    refs.modal.classList.remove("is-hidden");
+  };
+
+  function closeModal() {
+    window.removeEventListener("keydown", onEscapePress);
+    refs.modal.classList.add("is-hidden");
+  };
+
+
+
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      closeModal();
+    }
+  }
+
+  function onEscapePress(event) {
+    closeModal();
   }
 })();
